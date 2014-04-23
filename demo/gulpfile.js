7927c9e9b006203bcfc503864425c489493e836e
Tracy Rotton <tracy@taupecat.com>
2014-04-22 23:18:47 -0400

    progress…

---
+++ demo/gulpfile.js
@@ -0,0 +1,24 @@
+var gulp		= require('gulp'),
+	gutil		= require('gulp-util'),
+	sass		= require('gulp-ruby-sass'),
+	watch		= require('gulp-watch'),
+	notify		= require('gulp-notify'),
+	rename		= require('gulp-rename'),
+	minifycss	= require('gulp-minify-css'),
+	clean		= require('gulp-clean');
+
+gulp.task('clean', function() {
+	return gulp.src(['css'], {read: false})
+    .pipe(clean());
+});
+
+gulp.task('styles', function() {
+  return gulp.src('scss/**/*.scss')
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(gulp.dest('css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles');
+});
